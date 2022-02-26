be3de8c914cdb657045f2c2d75260394389525aa
SeaLorent <91618729+Sealorent@users.noreply.github.com>
2022-02-26 18:07:05 +0700

    50%

--- app/src/main/java/com/example/githublist/MainActivity.kt
+++ app/src/main/java/com/example/githublist/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.githublist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.AbsListView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var rvUserGit: RecyclerView
+    private val list = ArrayList<UserGit>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,12 +17,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-
-        val myName = resources.getStringArray(R.array.name)
-        val company = resources.getStringArray(R.array.company)
-        val location = resources.getStringArray(R.array.location)
-        val image = resources.getStringArray(R.array.avatar)
+        rvUserGit = findViewById(R.id.rvUserGit)
+        rvUserGit.setHasFixedSize(true)
 
 
+        list.addAll(listUserGit)
+        showRecyclerList()
+
+
+    }
+    private val listUserGit: ArrayList<UserGit>
+        get() {
+            val dataName = resources.getStringArray(R.array.name)
+            val dataCompany = resources.getStringArray(R.array.company)
+            val dataLocation = resources.getStringArray(R.array.location)
+            val dataPhoto = resources.obtainTypedArray(R.array.avatar)
+            val listUser = ArrayList<UserGit>()
+            for (i in dataName.indices) {
+                val user = UserGit(dataName[i],dataCompany[i],dataLocation[i], dataPhoto.getResourceId(i, -1))
+                listUser.add(user)
+            }
+            return listUser
+        }
+    private fun showRecyclerList() {
+        rvUserGit.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListUserGitAdapter(list)
+        rvUserGit.adapter = listHeroAdapter
     }
 }
--- app/src/main/java/com/example/githublist/UserGit.kt
+++ app/src/main/java/com/example/githublist/UserGit.kt
@@ -1,5 +1,42 @@
 package com.example.githublist
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class UserGit(
 
-)
+   var name: String?,
+   var Company: String?,
+   var location: String?,
+   var photo: Int
+
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(Company)
+        parcel.writeString(location)
+        parcel.writeInt(photo)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<UserGit> {
+        override fun createFromParcel(parcel: Parcel): UserGit {
+            return UserGit(parcel)
+        }
+
+        override fun newArray(size: Int): Array<UserGit?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
